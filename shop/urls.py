5013fce745042fc34eff0355d1b3101b14f4f269
balthaza77 <sheverovgleb@mail.ru>
2021-06-07 17:51:47 +0300

    New projects

---
+++ shop/urls.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.urls import include
+
+from . import views
+
+app_name = 'shop'
+app_name = 'orders'
+
+urlpatterns = [
+    url(r'^$', views.product_list, name='product_list'),
+    url(r'^(?P<category_slug>[-\w]+)/$',
+        views.product_list,
+        name='product_list_by_category'),
+    url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$',
+        views.product_detail,
+        name='product_detail'),
+    url(r'^orders/', include('orders.urls', namespace='orders')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
